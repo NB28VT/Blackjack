62555db76e1907e341456a56f7f5969b92d646c1
Nathan Burgess <nathan.burgess28@hotmail.com>
2014-12-02 13:45:49 -0500

    Created card hash

--- blackjack.rb
+++ blackjack.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Write a program where the user can play a game of **Blackjack**. Define and use the following classes to organize your code:
+# Write a program where the user can play a game of **Blackjack**. Define and use the following classes to organize your code=>
 #
 # * `Card` to represent an individual playing card. This class should contain the suit and the value and provide methods for determining what type of card it is (e.g. face card or ace).
 # * `Deck` to represent a collection of 52 cards. When dealing a hand this class can be used to supply the `Card` objects.
@@ -9,6 +9,136 @@
 
 require 'pry'
 
-spades = "U+2660(9824dec)".encode("UTF-8")	
+
 
 binding.pry
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Deck
+
+  def initialize(card_array= [])
+    @card_array = card_array
+    generte_deck
+  end
+
+  def generate_deck
+
+    # suits = [ '♠','♥', '♦', '♣' ]
+    # values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K']
+    #
+
+    card_hash = {
+      'A♠' => 1,
+      'A♥' => 1,
+      'A♦' => 1,
+      'A♣' => 1,
+      '2♠'=> 2,
+      '2♥'=> 2,
+      '2♦'=> 2,
+      '2♣'=> 2,
+      '3♠'=> 3,
+      '3♥'=> 3,
+      '3♦'=> 3,
+      '3♣'=> 3,
+      '4♠'=> 4,
+      '4♥'=> 4,
+      '4♦'=> 4,
+      '4♣'=> 4,
+      '5♠'=> 5,
+      '5♥'=> 5,
+      '5♦'=> 5,
+      '5♣'=> 5,
+      '6♠'=> 6,
+      '6♥'=> 6,
+      '6♦'=> 6,
+      '6♣'=> 6,
+      '7♠'=> 7,
+      '7♥'=> 7,
+      '7♦'=> 7,
+      '7♣'=> 7,
+      '8♠'=> 8,
+      '8♥'=> 8,
+      '8♦'=> 8,
+      '8♣'=> 8,
+      '9♠'=> 9,
+      '9♥'=> 9,
+      '9♦'=> 9,
+      '9♣'=> 9,
+      'J♠'=> 10,
+      'J♥'=> 10,
+      'J♦'=> 10,
+      'J♣'=> 10,
+      'Q♠'=> 10,
+      'Q♥'=> 10,
+      'Q♦'=> 10,
+      'Q♣'=> 10,
+      'K♠'=> 10,
+      'K♥'=> 10,
+      'K♦'=> 10,
+      'K♣'=> 10
+    }
+    
+
+
+
+
+
+
+
+    cards = values.product(suits)
+
+    cards.each do |card|
+      card_array << card.join()
+    end
+
+
+
+
+  end
+
+
+
+
+
+end
+
+
+class Card
+  def initialize(name, value)
+    @name = name
+    @value = value
+  end
+
+end
+
+
+
+
+class Hand
+  def initialize(player ,card1, card2)
+    @player = player
+    @card1 = card1
+    @card2 = card2
+  end
+end
