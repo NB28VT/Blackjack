6d56bb15ab62d14126a35aba3caf8a48fba1c24c
Nathan Burgess <nathan.burgess28@hotmail.com>
2014-12-02 11:42:41 -0500

    initial commit

---
+++ blackjack.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+#
+# Write a program where the user can play a game of **Blackjack**. Define and use the following classes to organize your code:
+#
+# * `Card` to represent an individual playing card. This class should contain the suit and the value and provide methods for determining what type of card it is (e.g. face card or ace).
+# * `Deck` to represent a collection of 52 cards. When dealing a hand this class can be used to supply the `Card` objects.
+# * `Hand` to represent the player's and dealer's hand. This class will need to determine the best score based on the cards that have been dealt.
+
+
+require 'pry'
+
+spades = "U+2660(9824dec)".encode("UTF-8")	
+
+binding.pry
