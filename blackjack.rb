0a1fac64afb2ec7b61605b56385f3046e214d87a
Daniel Bonnell <daniel.bonnell@gmail.com>
2014-12-02 14:36:29 -0500

    Test

--- blackjack.rb
+++ blackjack.rb
@@ -1,26 +1,17 @@
 #!/usr/bin/env ruby
-#
-# Write a program where the user can play a game of **Blackjack**. Define and use the following classes to organize your code=>
-#
-# * `Card` to represent an individual playing card. This class should contain the suit and the value and provide methods for determining what type of card it is (e.g. face card or ace).
-# * `Deck` to represent a collection of 52 cards. When dealing a hand this class can be used to supply the `Card` objects.
-# * `Hand` to represent the player's and dealer's hand. This class will need to determine the best score based on the cards that have been dealt.
-
-
 require 'pry'
 
 class Deck
-
   def initialize
-    generte_deck
+    @deck = []
+    @in_play
+    card_hash = {}
+    generate_deck
+    shuffle_deck
+    in_play
   end
 
   def generate_deck
-
-    # suits = [ '♠','♥', '♦', '♣' ]
-    # values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K']
-    #
-
     card_hash = {
       'A♠' => 1,
       'A♥' => 1,
@@ -72,58 +63,49 @@ class Deck
       'K♣'=> 10
     }
 
-    deck = []
-
     card_hash.each do |card, value|
-      deck << Card.new(card,value)
+      @deck << Card.new(card, value)
     end
+  end
 
-    binding.pry
+  def shuffle_deck
+    @deck.shuffle!
+  end
 
-
-
-
-  #
-  #
-  #   cards = values.product(suits)
-  #
-  #   cards.each do |card|
-  #     card_array << card.join()
-  #   end
-  #
-  #
-  #
-  #
-  # end
-
-
-
-
-
+  def self.in_play(card, value)
+    card_hash[:card] = value
+  end
 end
-
 
 class Card
-  def initialize(name, value)
-    @name = name
-    @value = value
+  def initialize
+    generate_card
   end
+
+  def generate_card
+    @card = @deck.sample
+    deck.in_play(@card.key, @card.val)
+  end
+
 end
-
-
-
 
 class Hand
-  def initialize(player ,card1, card2)
+  def initialize(player)
     @player = player
-    @card1 = card1
-    @card2 = card2
+    @card1 = Card.new(deck)
+    @card2 = Card.new(deck)
+    @hand = [@card1, @card2]
+  end
+
+  def hand
+    @hand
+  end
+
+  def draw
+    @hand << Card.new(deck)
   end
 end
 
+deck = Deck.new
 
-
-
-new_deck = Deck.new
-
-binding.pry
+Hand.new("Daniel")
