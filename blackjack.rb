cca51baf134973d9a522d5c859500785bcc7d46c
Daniel Bonnell <daniel.bonnell@gmail.com>
2014-12-05 11:57:34 -0500

    Refactored so that deck is dynamically generated and prompts are more understandable. Discovered a bug that occurs when a player's hand is dealt and the first card is an ace. The player must pick whether to set its value as 1 or 11. If the player chooses 11 and is subsequently dealt a Queen or King, they will loose the game automatically.

--- blackjack.rb
+++ blackjack.rb
@@ -1,14 +1,3 @@
-#!/usr/bin/env ruby
-#
-# Write a program where the user can play a game of **Blackjack**. Define and use the following classes to organize your code=>
-#
-# * `Card` to represent an individual playing card. This class should contain the suit and the value and provide methods for determining what type of card it is (e.g. face card or ace).
-# * `Deck` to represent a collection of 52 cards. When dealing a hand this class can be used to supply the `Card` objects.
-# * `Hand` to represent the player's and dealer's hand. This class will need to determine the best score based on the cards that have been dealt.
-
-
-require 'pry'
-
 class Deck
   attr_reader :deck, :in_play
 
@@ -17,74 +6,25 @@ class Deck
   end
 
   def generate_deck
-    card_hash = {
-      'A♠' => 1,
-      'A♥' => 1,
-      'A♦' => 1,
-      'A♣' => 1,
-      '2♠'=> 2,
-      '2♥'=> 2,
-      '2♦'=> 2,
-      '2♣'=> 2,
-      '3♠'=> 3,
-      '3♥'=> 3,
-      '3♦'=> 3,
-      '3♣'=> 3,
-      '4♠'=> 4,
-      '4♥'=> 4,
-      '4♦'=> 4,
-      '4♣'=> 4,
-      '5♠'=> 5,
-      '5♥'=> 5,
-      '5♦'=> 5,
-      '5♣'=> 5,
-      '6♠'=> 6,
-      '6♥'=> 6,
-      '6♦'=> 6,
-      '6♣'=> 6,
-      '7♠'=> 7,
-      '7♥'=> 7,
-      '7♦'=> 7,
-      '7♣'=> 7,
-      '8♠'=> 8,
-      '8♥'=> 8,
-      '8♦'=> 8,
-      '8♣'=> 8,
-      '9♠'=> 9,
-      '9♥'=> 9,
-      '9♦'=> 9,
-      '9♣'=> 9,
-      '10♠'=> 10,
-      '10♥'=> 10,
-      '10♦'=> 10,
-      '10♣'=> 10,
-      'J♠'=> 10,
-      'J♥'=> 10,
-      'J♦'=> 10,
-      'J♣'=> 10,
-      'Q♠'=> 10,
-      'Q♥'=> 10,
-      'Q♦'=> 10,
-      'Q♣'=> 10,
-      'K♠'=> 10,
-      'K♥'=> 10,
-      'K♦'=> 10,
-      'K♣'=> 10
-    }
-
+    suits = ['♠', '♥', '♦', '♣']
+    cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+    card_hash = {}
     @deck = []
 
+    suits.each do |suit|
+      cards.each_with_index do |card, index|
+        current = card.to_s + suit
+        card_hash[current] = index + 1
+      end
+    end
+
     card_hash.each do |card, value|
-      deck << Card.new(card,value)
+      deck << Card.new(card, value)
     end
 
     @deck.shuffle!
-
-    end
-
-    @in_play = []
+  end
 end
-
 
 class Card
   attr_reader :name, :value
@@ -93,6 +33,11 @@ class Card
     @name = name
     @value = value
   end
+
+  def ace_change_value
+    @value += 10
+  end
+
 end
 
 class Hand
@@ -102,12 +47,22 @@ class Hand
     @player_name = player_name
     @hand_array = []
     @play_deck = play_deck
-    2.times { draw_card(@player_name)}
+    2.times { draw_card(@player_name) }
   end
 
   def draw_card(player_name)
     @card = @play_deck.deck.pop
-    puts "#{player_name} were dealt #{@card.name}"
+    if player_name == "You"
+      puts "#{player_name} were dealt #{@card.name}"
+      if @card.name.split("").include?("A")
+        puts "Current value of Ace is 1. Set to 11? (Y/N):"
+        player_choice = gets.chomp.downcase
+
+        if player_choice == "y"
+          @card.ace_change_value
+        end
+      end
+    end
     @hand_array << @card
   end
 
@@ -120,69 +75,10 @@ class Hand
   end
 end
 
-
-
-################################################ GAME METHODS #################
-
-
-
-#
-# def player_deal(new_deck)
-#   player_hand = Hand.new(new_deck, "Me")
-#   player_total = player_hand.card_total
-#
-#
-#
-#
-# end
-#
-#
-# def dealer_t
-#   dealer_total = dealer_hand.card_total
-#
-# end
-#
-#
-# def turn
-#   player_hit_or_stand
-#   # dealer_hit_or_stand
-# end
-#
-# def player_hit_or_stand
-#
-#   total = @player_hand.card_total
-#
-#
-#
-#
-#
-#
-#   # logic that decides if player hits or stands
-# end
-#
-# def dealer_hit_or_stand
-#   # logic decides if dealer hits or stands
-#   # if hand count > 17?
-#   #   stand
-#   # else
-#   #   dealer_turn
-#   # end
-# end
-
-
-
-
-
-
-
-
-
-
-# !!!!!!!!!!!!!!!!!!!! ACES ARE 1 OR 11
-
 def play_game
   player_wins = false
   computer_wins = false
+  stand = false
 
   puts "Welcome to Blackjack!\n\n"
   new_deck = Deck.new
@@ -190,78 +86,48 @@ def play_game
   player_hand = Hand.new(new_deck, "You")
   dealer_hand = Hand.new(new_deck, "Dealer")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  until player_wins == true || computer_wins == true
+  until player_wins == true || computer_wins == true || stand == true
     player_total = player_hand.card_total
-
-
-
+    dealer_total = dealer_hand.card_total
 
     if player_total >= 22
       computer_wins = true
-      puts "Bust: total is #{player_total}, house wins"
-    elsif player_total == 21
-      puts "You win"
-      player_wins = true
+      puts "You busted! Your total is #{player_total}."
     else
-      puts "Your card total is: #{player_total}"
-      puts "Hit or stand (H/S):"
+      puts "Your card total is #{player_total}."
+      puts "Hit or stand? (H/S):"
 
       player_choice = gets.chomp.downcase
 
       if player_choice == "h"
         player_hand.draw_card("You")
+
+      elsif player_choice == "s"
+
+        if dealer_total >= 22
+          player_wins = true
+          puts "Dealer busts!"
+        elsif dealer_total <= 17
+          dealer_hand.draw_card("Dealer")
+        else
+          puts "Dealer stays."
+          stand = true
+        end
       end
     end
 
+  end
 
+  puts "Player total is #{player_total}."
+  puts "Dealer total is #{dealer_total}.\n\n"
 
-
-
-
-
-
-
+  if player_wins && !computer_wins
+    puts "Player wins!"
+  elsif computer_wins && !player_wins
+    puts "Computer wins!"
+  else
+    puts "It's a tie!"
   end
 end
 
-
-
-
 play_game
-
-
-
-# until player_wins == true || computer_wins == true
-#   player_hand = Hand.new(new_deck)
-#
-#   #binding.pry
-#
-#   puts "Hit or stand (H/S):"
-#
-#
-# end
-#
-#
-# if player_wins == true
-#   puts "Player wins!"
-# elsif computer_wins == true
-#   puts "Dealer wins!"
-# end
